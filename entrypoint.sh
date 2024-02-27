d36f36df574a10ac7f0c9cf7047711485683e12f
Seth Fischer <seth@fischer.nz>
2024-02-27 18:23:21 +1300

    fix: error building mfg-data

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ ato_project_path=$1
 
 git config --global --add safe.directory /github/workspace
 
+
+ato_version=$(ato --version)
+echo "::notice:: ato version ${ato_version}"
+
 cd "$ato_project_path"
 ato install
 ato build
