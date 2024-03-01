f289096a043905980c051ec599f0b86e8187dbe2
Seth Fischer <seth@fischer.nz>
2024-03-01 21:24:57 +1300

    feat: log kicad version as notice message

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,12 @@ ato_project_path=$1
 git config --global --add safe.directory /github/workspace
 
 ato_version=$(ato --version)
+kicad_version=$(kicad-cli --version)
 echo "::notice:: ato version: ${ato_version}"
+echo "::notice:: kicad-cli version: ${kicad_version}"
 
 cd "$ato_project_path"
+echo "::notice:: starting ato install"
 ato install
+echo "::notice:: starting ato build"
 ato build
